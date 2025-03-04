07abe5c432b813ffce9e5eeedf9f82e07bb2a6c3
rikardbq <j10rikardb@gmail.com>
2025-03-04 19:39:53 +0100

    add maven build assymbly plugin conf to generate a jar with deps

--- src/main/java/se/rikardbq/Main.java
+++ src/main/java/se/rikardbq/Main.java
@@ -4,6 +4,13 @@ import se.rikardbq.connector.Connector;
 import se.rikardbq.connector.Migrator;
 import se.rikardbq.temp.SomeDataClass;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
 import java.util.List;
 
 public class Main {
@@ -22,7 +29,23 @@ public class Main {
             long rowsAffected = conn.mutate("INSERT INTO testing_table(im_data, im_data_also, im_data_too) VALUES(?, ?, ?)", "teeeeeeee8888888", "heeeeeeeeee", "wwaaaaaaaaaaa");
             System.out.println("DATA===== " + data);
             System.out.println("DATA_ROWS_AFFECTED===== " + rowsAffected);
-            migrator.run(conn);
+//            migrator.run(conn);
+
+//            var document = DocumentBuilderFactory
+//                    .newInstance()
+//                    .newDocumentBuilder()
+//                    .newDocument();
+//
+//            var testingParent = document.createElement("TestingParent");
+//
+//            testingParent.setTextContent("hello");
+//            document.appendChild(testingParent);
+//            var src = new DOMSource(document);
+//            var str = new StreamResult(System.out);
+//
+//            var trs = TransformerFactory.newInstance().newTransformer();
+//            trs.transform(src, str);
+
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
