08e918503fc8f7500f6e384cacec1f449e37fef5
rikardbq <j10rikardb@gmail.com>
2025-03-05 10:08:08 +0100

    update deploy scripts again

--- deploy_artifacts.bat
+++ deploy_artifacts.bat
@@ -1,7 +1,7 @@
 @echo off & setlocal
 
 FOR /F "tokens=*" %%a in ('git rev-list HEAD --count') do SET build_number=%%a
-mvn -U versions:set -DnewVersion=%build_number% \
+mvn -U versions:set -DnewVersion=%build_number% ^
 && mvn clean deploy
 
 exit /b
