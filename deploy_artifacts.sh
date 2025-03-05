8d2e33f37d99b62ade948bcf2c0798833b0eae12
rikardbq <j10rikardb@gmail.com>
2025-03-05 10:05:03 +0100

    update deploy scripts

--- deploy_artifacts.sh
+++ deploy_artifacts.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 build_version=$(git rev-list HEAD --count)
-mvn -U versions:set -DnewVersion=${build_version}
-mvn clean deploy
+mvn -U versions:set -DnewVersion=${build_version} \
+&& mvn clean deploy
 
 exit 0
