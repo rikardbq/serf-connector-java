80e21dde260e42787b49c0edafce7fc7743c2de2
rikardbq <j10rikardb@gmail.com>
2025-03-19 19:32:18 +0100

    fix random thing + up version

--- src/main/java/se/rikardbq/models/migration/Migration.java
+++ src/main/java/se/rikardbq/models/migration/Migration.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 import java.util.Objects;
 
 public class Migration implements Serializable {
-    
+
     @Serial
     private static final long serialVersionUID = 1L;
 
