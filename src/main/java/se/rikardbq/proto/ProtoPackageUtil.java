77e8576b4a29c540892014e5b04a586ae2af1319
rikardbq <j10rikardb@gmail.com>
2025-03-31 10:08:22 +0200

    remove double hash step that was there by mistake lol

--- src/main/java/se/rikardbq/proto/ProtoPackageUtil.java
+++ src/main/java/se/rikardbq/proto/ProtoPackageUtil.java
@@ -1,6 +1,6 @@
 package se.rikardbq.proto;
 
-import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.codec.binary.Hex;
 import org.apache.commons.codec.digest.HmacAlgorithms;
 import org.apache.commons.codec.digest.HmacUtils;
 
@@ -13,6 +13,6 @@ public class ProtoPackageUtil {
         mac.update(data);
         byte[] result = mac.doFinal();
 
-        return DigestUtils.sha256Hex(result);
+        return Hex.encodeHexString(result);
     }
 }
